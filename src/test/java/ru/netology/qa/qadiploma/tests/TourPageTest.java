5565860ad768495d3e61a20b32640659e64f0827
nshabankin <n.shabankin@gmail.com>
2024-09-12 17:55:24 +0300

    Add DataGenerator class and expand basic tests

--- src/test/java/ru/netology/qa/qadiploma/tests/TourPageTest.java
+++ src/test/java/ru/netology/qa/qadiploma/tests/TourPageTest.java
@@ -3,6 +3,7 @@ package ru.netology.qa.qadiploma.tests;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import ru.netology.qa.qadiploma.data.DataGenerator;
 import ru.netology.qa.qadiploma.data.TestData;
 import ru.netology.qa.qadiploma.pages.TourPageElements;
 import ru.netology.qa.qadiploma.utils.TourPageActions;
@@ -14,21 +15,20 @@ public class TourPageTest {
 
     private TourPageElements elements;  // Класс, который хранит элементы страницы
     private TourPageActions actions;    // Класс для выполнения действий на странице
-    private TestData testData;          // Тестовые данные для покупки
+    private TestData approvedUserData;  // Тестовые данные для успешной покупки
+    private TestData declinedUserData;  // Тестовые данные для неуспешной покупки
+    private int sleepTime = 15000; // Время задержки
 
     @BeforeEach
     public void setup() {
         elements = new TourPageElements();  // Инициализация элементов страницы
         actions = new TourPageActions(elements);  // Инициализация действий с элементами
 
-        // Создание объекта с тестовыми данными для покупки по дебетовой карте
-        testData = new TestData(
-                "4444 4444 4444 4441",
-                "12",
-                "24",
-                "Ivan Ivanov",
-                "123"
-        );
+        // Создание объекта с тестовыми данными для успешной покупки
+        approvedUserData = DataGenerator.getApprovedUserData();
+
+        // Создание объекта с тестовыми данными для неуспешной покупки
+        declinedUserData = DataGenerator.getDeclinedUserData();
     }
 
     @SneakyThrows
@@ -38,15 +38,69 @@ public class TourPageTest {
         actions.selectDebitCardPayment();  // Выбираем оплату по дебетовой карте
 
         // Передаем тестовые данные в методы
-        actions.enterCardNumber(testData.getCardNumber());  // Вводим номер карты
-        actions.enterCardMonth(testData.getCardMonth());  // Вводим месяц
-        actions.enterCardYear(testData.getCardYear());  // Вводим год
-        actions.enterCardHolderName(testData.getCardHolderName());  // Вводим имя владельца
-        actions.enterCardCvv(testData.getCardCvv());  // Вводим CVC/CVV код
+        actions.enterCardNumber(approvedUserData.getCardNumber());  // Вводим номер карты
+        actions.enterCardMonth(approvedUserData.getCardMonth());  // Вводим месяц
+        actions.enterCardYear(approvedUserData.getCardYear());  // Вводим год
+        actions.enterCardHolderName(approvedUserData.getCardHolderName());  // Вводим имя владельца
+        actions.enterCardCvv(approvedUserData.getCardCvv());  // Вводим CVC/CVV код
 
         actions.submitPurchase();  // Нажимаем на кнопку "Продолжить"
-        Thread.sleep(10000); // Задержка в миллисекундах
+        Thread.sleep(sleepTime); // Задержка в миллисекундах
         actions.checkSuccessNotification();  // Проверяем успешное завершение покупки
+    }
+
+    @SneakyThrows
+    @Test
+    public void testUnsuccessfulDebitCardPurchase() {
+        actions.openPage();  // Открываем страницу
+        actions.selectDebitCardPayment();  // Выбираем оплату по дебетовой карте
+
+        // Передаем тестовые данные в методы
+        actions.enterCardNumber(declinedUserData.getCardNumber());  // Вводим номер карты
+        actions.enterCardMonth(declinedUserData.getCardMonth());  // Вводим месяц
+        actions.enterCardYear(declinedUserData.getCardYear());  // Вводим год
+        actions.enterCardHolderName(declinedUserData.getCardHolderName());  // Вводим имя владельца
+        actions.enterCardCvv(declinedUserData.getCardCvv());  // Вводим CVC/CVV код
+
+        actions.submitPurchase();  // Нажимаем на кнопку "Продолжить"
+        Thread.sleep(sleepTime); // Задержка в миллисекундах
+        actions.checkErrorNotification();  // Проверяем неуспешное завершение покупки
+    }
+
+    @SneakyThrows
+    @Test
+    public void testSuccessfulCreditPurchase() {
+        actions.openPage();  // Открываем страницу
+        actions.selectCreditPayment();  // Выбираем оплату кредитом
+
+        // Передаем тестовые данные в методы
+        actions.enterCardNumber(approvedUserData.getCardNumber());  // Вводим номер карты
+        actions.enterCardMonth(approvedUserData.getCardMonth());  // Вводим месяц
+        actions.enterCardYear(approvedUserData.getCardYear());  // Вводим год
+        actions.enterCardHolderName(approvedUserData.getCardHolderName());  // Вводим имя владельца
+        actions.enterCardCvv(approvedUserData.getCardCvv());  // Вводим CVC/CVV код
+
+        actions.submitPurchase();  // Нажимаем на кнопку "Продолжить"
+        Thread.sleep(sleepTime); // Задержка в миллисекундах
+        actions.checkSuccessNotification();  // Проверяем успешное завершение покупки
+    }
+
+    @SneakyThrows
+    @Test
+    public void testUnsuccessfulCreditPurchase() {
+        actions.openPage();  // Открываем страницу
+        actions.selectCreditPayment();  // Выбираем оплату кредитом
+
+        // Передаем тестовые данные в методы
+        actions.enterCardNumber(declinedUserData.getCardNumber());  // Вводим номер карты
+        actions.enterCardMonth(declinedUserData.getCardMonth());  // Вводим месяц
+        actions.enterCardYear(declinedUserData.getCardYear());  // Вводим год
+        actions.enterCardHolderName(declinedUserData.getCardHolderName());  // Вводим имя владельца
+        actions.enterCardCvv(declinedUserData.getCardCvv());  // Вводим CVC/CVV код
+
+        actions.submitPurchase();  // Нажимаем на кнопку "Продолжить"
+        Thread.sleep(sleepTime); // Задержка в миллисекундах
+        actions.checkErrorNotification();  // Проверяем неуспешное завершение покупки
     }
 }
 
